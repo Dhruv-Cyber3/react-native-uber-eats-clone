cb0dfc5db4136f6e1d15cba53334db1385a871dc
Dhruv Choudhary <70338818+Dhruv-Cyber3@users.noreply.github.com>
2022-01-19 17:50:25 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
-
+const end="iU676afbdba";
 console.ignoredYellowBox = ["Setting a timer"];
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKjYCGT1uN9ftl2MV0beu6ifmwmp8yTiU",
+  apiKey: "AIzaSyCKjYCGT1uN9ftl2MV0beu6ifmwmp8yT",
   authDomain: "react-native-uber-eats-c-886f4.firebaseapp.com",
   projectId: "react-native-uber-eats-c-886f4",
   storageBucket: "react-native-uber-eats-c-886f4.appspot.com",
-  messagingSenderId: "1009910428367",
-  appId: "1:1009910428367:web:529ca3e6614b0df6afbdba",
+  messagingSenderId: "10099104283",
+  appId: "1:1009910428367:web:529ca3e6614b0df",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
